014dca243e8259348c8ed7f95d530cd3756152e3
Liu <1219062025@qq.com>
2021-04-15 08:27:32 +0800

    提交项目

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar centerColor="#fff">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home.js";
 export default {
   name: "Home",
+  components: {
+    NavBar,
+  },
+  created() {
+    getHomeMultidata();
+  },
 };
 </script>
 
